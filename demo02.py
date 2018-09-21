578fdd98ee43b50d3e762489cceba607a06cddaf
ewfrer <guohan-gj@qq.com>
2018-09-21 22:05:24 +0800

    dfdfdf

---
+++ demo02.py
@@ -0,0 +1,27 @@
+
+
+from multiprocessing import JoinableQueue,Process
+
+def customer (queue) :
+    while True:
+        r = queue.get()
+        print("消费:" + r)
+        queue.task_done() # 消费完成, 通知继续生产
+
+
+def product(queue,name) :
+    for i in range(10):
+        queue.put(name)
+        print("生产骨头")
+        queue.join() # 阻塞当前,让队列中其他执行
+
+if __name__ == '__main__':
+    queue = JoinableQueue()
+    cus = Process(target=customer,args=(queue,))
+    cus.daemon = True # 设置开启之前, 一旦进程开启了, 设置不好使
+    pro = Process(target=product,args=(queue,"骨头"))
+    cus.start()
+    pro.start()
+
+    cus.join() # 使用join 来保证 守护进程没有挂掉
+
